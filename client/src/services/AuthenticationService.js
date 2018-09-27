54cdddedbfb5e054768788ef29529c50e38d7978
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2018-09-27 12:42:39 +0200

    Small changes

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -1,12 +1,12 @@
 import Api from '@/services/Api'
 
 export default {
-	register (credentials) {
-		return Api().post('register', credentials)
-	}
+  register (credentials) {
+    return Api().post('register', credentials)
+  }
 }
 
 // AuthenticationService.register({
-// 	email: 'testing@gmail.com',
-// 	password: '123456'
+//  email: 'testing@gmail.com',
+//  password: '123456'
 // })
